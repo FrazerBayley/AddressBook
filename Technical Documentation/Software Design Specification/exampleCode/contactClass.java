a6ed273f4afb754158d5573ef2ff17e59d9fec4d
Haley Whitman <hwhitman@uoregon.edu>
2017-01-25 17:49:46 -0800

    Updated SDS with updates to contact class

--- Technical Documentation/Software Design Specification/exampleCode/contactClass.java
+++ Technical Documentation/Software Design Specification/exampleCode/contactClass.java
@@ -1,11 +1,2 @@
 public Contact(String fn, String ln, String phone, String email, 
-		String add, String city, String state, String zip) {
-	_firstName = fn;
-	_lastName = ln;
-	_phone = phone;
-	_email = email;
-	_address = add;
-	_city = city;
-	_state = state;
-	_zip = zip;
-}
+		String add, String city, String state, String zip) { }
